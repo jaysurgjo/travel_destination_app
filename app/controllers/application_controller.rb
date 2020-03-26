6fcd3d3d5ac2fe625d70336929b275bd00b1e6d9
github username <github email address>
2020-03-26 09:01:02 -0400

    added to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,27 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  def logged_in?
-        !!session[:user_id]
-      end
-
-      def current_user
-        User.find(session[:user_id])
-      end
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user.nil?
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect to '/account'
     end
+  end
+
+  get '/account' do
+    @user = User.find_by_id(session[:user_id])
+    if @user.nil?
+      erb :error
+    else
+      erb :account
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+end
