3fb782092c10e0aee655abbfc527fb9db46966b0
github username <github email address>
2020-04-19 22:53:01 -0400

    added to dest controller again

--- app/controllers/destination_controller.rb
+++ app/controllers/destination_controller.rb
@@ -5,7 +5,11 @@ class DestinantionController < ApplicationController
   end
 
   get "/destinations/new" do
-    ""
+    if @destinations.nil?
+      "You must enter a valid location"
+    else
+      erb :'destinations/new'
+    end
   end
 
   get "/destinations/:id/edit" do
